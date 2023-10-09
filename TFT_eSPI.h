aae7891ece61afc413179b223c649a5256295981
Roman Mashta <roman.mashta@gmail.com>
2023-10-10 01:11:40 +0300

    Remove inlining for native

--- TFT_eSPI.h
+++ TFT_eSPI.h
@@ -734,7 +734,7 @@ class TFT_eSPI : public Print { friend class TFT_eSprite; // Sprite class has ac
            // Alpha blend 2 colours, see generic "alphaBlend_Test" example
            // alpha =   0 = 100% background colour
            // alpha = 255 = 100% foreground colour
-#if !defined (STM32) && !defined(__IMXRT1052__) && !defined(__IMXRT1062__)
+#if !defined (STM32) && !defined(__IMXRT1052__) && !defined(__IMXRT1062__) && !defined (ARCH_NATIVE)
   inline 
 #endif
   uint16_t alphaBlend(uint8_t alpha, uint16_t fgc, uint16_t bgc);
